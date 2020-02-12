3c8e2a4c78562803d1b06f1319d7af70ed449933
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-12 21:56:44 +0000

    Automatically backed up by Learn

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :genre, through: :song_genres
+
+  attr_accessor :genre_ids
+
+  def genre_ids=(genre_id)
+    if !@genre_ids
+      @genre_ids = []
+    end
+    @genre_ids << genre_id
+  end
+end
