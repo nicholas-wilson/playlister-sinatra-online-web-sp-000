3ce7deefe4d91d8f2079cb25e9522d48e17b50be
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 01:17:38 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -7,6 +7,15 @@ module Slugifiable
   end
 
   module ClassMethods
-
+    def self.find_by_slug(slug)
+      obj_name = slug.gsub(/-/, " ")
+      obj = nil
+      Artist.all.each do |artist|
+        if artist.name.downcase == artist_name
+          artist_obj = artist
+        end
+      end
+      artist_obj
+    end
   end
 end
