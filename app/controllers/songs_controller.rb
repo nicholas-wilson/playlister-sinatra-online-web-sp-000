f3416ba87a9e554620b31b27ba8930340adccf74
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 02:03:28 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+
+  end
+
+  get '/songs/new' do
+
+  end
+
+  get '/songs/:id' do
+
+  end
+
+  get '/songs/:id/edit' do
+
+  end
+
+  post '/songs' do
+
+  end
+
+  patch '/songs/:id' do
+
+  end
+
+  delete '/songs/:id' do
+
+  end
+end
