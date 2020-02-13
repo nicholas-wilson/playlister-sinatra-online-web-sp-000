333d1650bccc9791942c0963afd8dc82f5af1bbe
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 01:24:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,8 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
 
   attr_accessor :genre_ids
 
