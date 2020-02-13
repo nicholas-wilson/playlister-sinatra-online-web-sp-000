0c2f07c8ac32c058fe10f237bdfd32528dfd79de
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 02:24:27 +0000

    Automatically backed up by Learn

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,31 @@
+class GenresController < ApplicationController
+
+  get '/genres' do
+    @genres = Song.all
+    erb :"/genre/index"
+  end
+
+  get '/genres/new' do
+
+  end
+
+  get '/genres/:slug' do
+
+  end
+
+  get '/genres/:slug/edit' do
+
+  end
+
+  post '/genres' do
+
+  end
+
+  patch '/genres/:slug' do
+
+  end
+
+  delete '/genres/:slug' do
+
+  end
+end
