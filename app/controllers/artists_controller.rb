0c0f3f35733f99af6bfb0a77e66f09d0a6899455
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 01:55:21 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,28 @@
 class ArtistsController < ApplicationController
 
+  get '/artists' do
+
+  end
+
+  get '/artists/new' do
+
+  end
+
+  get '/artists/:id' do
+
+  end
+
+  get '/artists/:id/edit' do
+
+  end
+
+  post '/artists' do
+
+  end
+
+  patch '/artists/:id'
+
+  delete '/artists/:id' do
+
+  end
 end
