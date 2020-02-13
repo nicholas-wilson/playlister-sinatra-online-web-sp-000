d1d0037869f672dc8e396feadfc52816e0663caa
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-13 01:21:24 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,14 +9,14 @@ class Artist < ActiveRecord::Base
   #   sluggy.gsub(/\s+/, "-").downcase
   # end
 
-  def self.find_by_slug(slug)
-    artist_name = slug.gsub(/-/, " ")
-    artist_obj = nil
-    Artist.all.each do |artist|
-      if artist.name.downcase == artist_name
-        artist_obj = artist
-      end
-    end
-    artist_obj
-  end
+  # def self.find_by_slug(slug)
+  #   artist_name = slug.gsub(/-/, " ")
+  #   artist_obj = nil
+  #   Artist.all.each do |artist|
+  #     if artist.name.downcase == artist_name
+  #       artist_obj = artist
+  #     end
+  #   end
+  #   artist_obj
+  # end
 end
